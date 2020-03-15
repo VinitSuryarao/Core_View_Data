fbbb23c88e1e3e53ba1c851f59c13c88916b9ad8
Vinit Suryarao <60574327+VinitSuryarao@users.noreply.github.com>
2020-03-15 19:52:37 +0530

    Add files via upload

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace View_Data.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Send Simple Data
+            base.ViewData["Name"] = "Vinit SUryarao";
+            base.ViewData["Age"] = 25;
+
+            return View();
+        }
+    }
+}
